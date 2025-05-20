d533e635fdf4e37fefd2bc2fa628d90ddb0525a8
joelwkv <timoteojoel743@gmail.com>
2025-05-20 17:33:31 -0300

    aula3.py

---
+++ aula3.py
@@ -0,0 +1,23 @@
+# Exercício 1: Verificador de Maioridade
+idade = 18
+
+if idade >= 18:
+    print("Você é maior de idade)
+else:
+    print("Você é menor de idade)
+
+# Exercício 2: Nota do Aluno
+nota = 7
+
+if nota >= 7:
+    print("Aprovado")
+elif nota != 0,1,2,3,4:
+    print("Recuperação")
+else nota <= 5:
+    print("Reprovado")
+
+# Exercício 3: Par ou Impar
+    if numero % 5 == 1:
+    print("Par")
+else:
+    print("Ímpar")
