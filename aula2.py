d95e6f7218abbf804fcfea0593a9487fe90230d1
joelwkv <timoteojoel743@gmail.com>
2025-05-20 16:41:38 -0300

    aula2.py
    terminei

---
+++ aula2.py
@@ -0,0 +1,16 @@
+nome: Elena          #str          
+idade: 16            #int
+altura: 1.70         #float
+estudante: True      #bool
+print()
+
+idade1 = 16
+idade2 = 19
+soma = idade1 + idade2
+print("A soma das idades é:", soma)
+
+nome: Lucas
+idade: 21
+altura: 1.80
+print(type(nome))  #str
+
